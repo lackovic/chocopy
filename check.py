ef5a1a8bee60fd82ac4e7e00acf53e5a099af393
Marco Lackovic <marco.lackovic@gmail.com>
2020-02-10 00:03:51 +0200

    Add bulkrenameutility PackageRoutine

--- check.py
+++ check.py
@@ -34,12 +34,4 @@ def WriteVersionFile(name, version, url):
     return
 
 # Running the routines
-PackageRoutine("homebank", "http://homebank.free.fr/downloads.php", r'/public/HomeBank-([\w\.\-]*)-setup.exe', 'http://homebank.free.fr/public/HomeBank-{{version}}-setup.exe')
-
-PackageRoutine("darktable", "https://github.com/darktable-org/darktable/releases/latest", r'release-[\w\.\-]*\/darktable-([\w\.\-]*)-win64.exe', "https://github.com/darktable-org/darktable/releases/download/release-{{version}}/darktable-{{version}}-win64.exe")
-
-PackageRoutine("microsoft-r-open", "https://github.com/Microsoft/microsoft-r-open/releases/latest", r'https://github.com/Microsoft/microsoft-r-open/commits/MRO-([\w\.\-]*).atom', "https: //mran.blob.core.windows.net/install/mro/{{version}}/windows/microsoft-r-open-{{version}}.exe")
-
-PackageRoutine("rawtherapee", "http://rawtherapee.com/downloads/", r'\/shared\/builds\/windows\/RawTherapee_([\w\.]*).exe', "https://rawtherapee.com/shared/builds/windows/RawTherapee_{{version}}.exe")
-
-PackageRoutine("terminus", "https://github.com/Eugeny/terminus/releases/latest", r'terminus\/releases\/download\/v[\w\.\-]*\/terminus-([\w\.\-]*)-setup.exe', "https://github.com/Eugeny/terminus/releases/download/v{{version}}/terminus-{{version}}-setup.exe")
+PackageRoutine("bulkrenameutility", "https://www.bulkrenameutility.co.uk/Download.php", r'(?<=<span class="text-muted">version )(.*)(?=<\/span>)', "https://www.bulkrenameutility.co.uk/Downloads/BRU_setup.exe")
